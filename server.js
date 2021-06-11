62af0c96c9a253ba2767f475b513b830e673d023
Antonio <ws_17@jogroup.eu>
2021-06-11 14:58:54 +0200

    sync

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+
+app.get('/api/users', (req, res) =>{
+
+    res.json({
+        users: [
+            {name : 'Antonio'},
+            {name : 'Andrea'},
+        ]
+        
+    });
+
+} );
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
