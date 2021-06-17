f8757f949670b603bf1b37993c002e31c4779b64
Antonio <ws_17@jogroup.eu>
2021-06-17 14:27:42 +0200

    sync

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const path = require('path');
-var cors = require('cors');
+const cors = require('cors');
 
 
 const authorize = require('./Server/helpers/authorize')
