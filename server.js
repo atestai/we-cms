5169077c18009dbcf3e75f524811c0ad01d4f324
Antonio <ws_17@jogroup.eu>
2021-06-22 12:09:47 +0200

    sync

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const authorize = require('./Server/helpers/authorize')
 const app = express();
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
