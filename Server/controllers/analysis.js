df11ae0713c1d8ff320360fb1b0a17b51b507cb4
Antonio <ws_17@jogroup.eu>
2022-01-28 11:39:12 +0100

    sync

--- Server/controllers/analysis.js
+++ Server/controllers/analysis.js
@@ -5,18 +5,17 @@ const { Op } = require("sequelize");
 // const constants = require('../helpers/constants');
 // const UserModel = require('../models').User;
 
-
 const router = express.Router();
-
 
 const Analysis = {
 
     async create(req, res) {
         console.log('user-create');
 
+        res.json({
+            'user-create' : 'user-create'
+        })
     }
-
 }
-
 
 router.post('/', Analysis.create);
